35c0f16ead2b2cc0616db4825757b65f5130063a
HannaWang003 <hannawang003@gmail.com>
2024-02-24 15:53:38 +0800

    練習db檔

--- index.php
+++ index.php
@@ -15,7 +15,6 @@
 	<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
 		<pre id="ssaa"></pre>
 	</div>
-	<iframe name="back" style="display:none;"></iframe>
 	<div id="all">
 		<div id="title">
 			00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 
